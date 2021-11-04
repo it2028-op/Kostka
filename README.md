054cc981ba98301e4594d792695c64173b2b810b
it2028-op <svanus@seznam.cz>
2021-11-04 20:01:30 +0100

    1/a.0 - projekt alfa

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 "Kostka" 
 "0.1 - Vzor"
 "0.2 - Vzor-complete"
+"1/a.0 -projekt alfa"
--- js/game.js
+++ js/game.js
@@ -1,32 +1,124 @@
-const kostka = document.getElementById('cube');
+const num1 = document.getElementById('cube1');
+const num2 = document.getElementById('cube2');
+const num3 = document.getElementById('cube3');
+const num4 = document.getElementById('cube4');
+const num5 = document.getElementById('cube5');
+const num6 = document.getElementById('cube6');
 const stat = document.getElementById('result');
 const buttonek = document.getElementById('playb');
 
-let hod = 1;
-let hody = [];
+let hod1 = 1;
+let hod2 = 1;
+let hod3 = 1;
+let hod4 = 1;
+let hod5 = 1;
+let hod6 = 1;
+let hody = 0;
 let timer = false;
+let final = 0;
+let helpf = [];
+let scor = 0;
 
 function animace() {
-    hod = Math.ceil(Math.random() * 6);
-    kostka.src = 'img/kostka' + hod + '.png';
+    hod1 = Math.ceil(Math.random() * 6);
+    hod2 = Math.ceil(Math.random() * 6);
+    hod3 = Math.ceil(Math.random() * 6);
+    hod4 = Math.ceil(Math.random() * 6);
+    hod5 = Math.ceil(Math.random() * 6);
+    hod6 = Math.ceil(Math.random() * 6);
+    num1.src = 'img/kostka' + hod1 + '.png';
+    num2.src = 'img/kostka' + hod2 + '.png';
+    num3.src = 'img/kostka' + hod3 + '.png';
+    num4.src = 'img/kostka' + hod4 + '.png';
+    num5.src = 'img/kostka' + hod5 + '.png';
+    num6.src = 'img/kostka' + hod6 + '.png';
 }
 
-buttonek.addEventListener('click', function(){
+buttonek.addEventListener('click', function () {
     if (!timer) {
+        let helpf = [];
         timer = setInterval(animace, 50);
         buttonek.innerText = 'STOP';
+        hody++;
     } else {
         clearInterval(timer);
         timer = false;
         buttonek.innerText = 'PLAY'
-        hody.push(hod);
+        //hody.push(hod);
         vypisStat();
+        console.log(helpf);
+        helpf = [0];
     }
-
-    hody.push(hod);
-    console.log(hody);
-    vypisStat();
 })
+
+function score(scor) {
+    if (hod1 = 1) {
+        scor += 100;
+    }
+    else if (hod1 = 5) {
+        scor += 50;
+    }
+    else {
+        helpf.push(hod1);
+    }
+    if (hod2 = 1) {
+        scor += 100;
+    }
+    else if (hod2 = 5) {
+        scor += 50;
+    }
+    else {
+        helpf.push(hod2);
+    }
+    if (hod3 = 1) {
+        scor += 100;
+    }
+    else if (hod3 = 5) {
+        scor += 50;
+    }
+    else {
+        helpf.push(hod3);
+    }
+    if (hod4 = 1) {
+        scor += 100;
+    }
+    else if (hod4 = 5) {
+        scor += 50;
+    }
+    else {
+        helpf.push(hod4);
+    }
+    if (hod5 = 1) {
+        scor += 100;
+    }
+    else if (hod5 = 5) {
+        scor += 50;
+    }
+    else {
+        helpf.push(hod5);
+    }
+    if (hod6 = 1) {
+        scor += 100;
+    }
+    else if (hod6 = 5) {
+        scor += 50;
+    }
+    else {
+        helpf.push(hod6);
+    }
+return scor;
+}
+
+/*function porovnavani() {
+    for (let i = 1; i < 6; i++) {
+        if (helpf[i] == helpf[i2]) {
+            iMax = i;
+        }
+        else
+    }
+    return iMax;
+}
+}
 
 function suma() {
     let sum = 0;
@@ -51,12 +143,18 @@ function mi() {
     })
     return miHelp;
 }
+*/
 
-function vypisStat(){
+function vypisStat() {
+    /*
     stat.innerHTML = `<p>Posledni hod: ${hod}</p>`;
     stat.innerHTML += `<p>Počet hodů: ${hody.length}</p>`;
     stat.innerHTML += `<p>Součet: ${suma()}</p>`;
     stat.innerHTML += `<p>Průměr: ${(suma() / hody.length).toFixed(2)}</p>`;
     stat.innerHTML += `<p>Maximum: ${mx()}</p>`;
     stat.innerHTML += `<p>Minimum: ${mi()}</p>`;
+    */
+    stat.innerHTML += '<p>Hodnoty hodů: Hod 1: ' + hod1 + ' || ' + ' Hod 2: ' + hod2 + ' || ' + ' Hod 3: ' + hod3 + ' || ' + ' Hod 4: ' + hod4 + ' || ' + ' Hod 5: ' + hod5 + ' || ' + ' Hod 6: ' + hod6 + ' || ' + '</p>';
+    stat.innerHTML += `<p>Počet hodů: ${hody}</p>`;
+    stat.innerHTML += '<p>Skóre: ' + scor +'</p>';
 }
